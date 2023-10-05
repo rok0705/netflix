fe4d8be5a71ef9bc9e61301f9c8690e754dcd518
rok0705 <leeseon0503@gmail.com>
2023-10-05 00:42:36 -0400

    setup router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
+
+// roadmap
+// 1. pages: homePage, movies page, movie detail page
+// 2. homepage shows banner
+// 3. homepage shows 3 movie sections. popular, top rated, upcoming
+// 4. on mousehover movie, show title, genre, score, popularity, adult
+// 5. movie slider window
+
+// 6. In movie detail page, show poster, detail info etc.
+// 7. on click trailer, show trailer clip
+// 8. can see movie review
+// 9. can see related movies
+
+// 10. movie search bar on homepage.
+// 11. on search result, can sort and filter.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/movies/:id" element={<MovieDetail />}></Route>
+      </Routes>
     </div>
   );
 }
