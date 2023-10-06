c3b26013bea6cc9f39f55ce38855be77b290a841
rok0705 <leeseon0503@gmail.com>
2023-10-06 12:32:31 -0400

    create 3 movie slide frames using react-multi-carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { movieAction } from "../redux/actions/MovieAction";
 import { useDispatch, useSelector } from "react-redux";
 import Banner from "../components/Banner";
+import MovieSlide from "../components/MovieSlide";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -17,6 +18,12 @@ const Home = () => {
   return (
     <div>
       {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+      <h1>Popular Movies</h1>
+      <MovieSlide movies={popularMovies}></MovieSlide>
+      <h1>Top Rated Movies</h1>
+      <MovieSlide movies={topRatedMovies}></MovieSlide>
+      <h1>Upcoming Movies</h1>
+      <MovieSlide movies={upcomingMovies}></MovieSlide>
     </div>
   );
 };
