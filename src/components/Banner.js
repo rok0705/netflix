9cc3c7e55bb599382244231ee674b1cdda0d7411
rok0705 <leeseon0503@gmail.com>
2023-10-06 11:06:11 -0400

    finish banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Banner = ({ movie }) => {
-  return <div>{movie.original_title}</div>;
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces/${movie.poster_path}` +
+          ")",
+      }}
+    >
+      <div className="banner-info">
+        <h1 className="banner-title">{movie.title}</h1>
+        <p className="banner-description">{movie.overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
